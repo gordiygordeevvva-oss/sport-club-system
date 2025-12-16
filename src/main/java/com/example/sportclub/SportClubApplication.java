71214be641a282c00bd8bb3b4104447ad479fafc
gordiygordeevvva-oss <gordiygordeevvv@gmail.com>
2025-12-16 18:37:12 +0300

    Create SportClubApplication.java

---
+++ src/main/java/com/example/sportclub/SportClubApplication.java
@@ -0,0 +1,9 @@
+package com.example.sportclub;
+
+public class SportClubApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Информационно-справочная система спортивного клуба");
+    }
+}
+
