528ed8c72bc83cc5222b8d928bcb79203ac01096
gordiygordeevvva-oss <gordiygordeevvv@gmail.com>
2025-12-16 23:53:36 +0300

    Update ClubController.java
    Обновлены данные тренеров и расписания

--- src/main/java/com/example/sportclub/src/main/java/com/example/sportclub/model/src/main/java/com/example/sportclub/model/src/main/java/com/example/sportclub/controller/ClubController.java
+++ src/main/java/com/example/sportclub/src/main/java/com/example/sportclub/model/src/main/java/com/example/sportclub/model/src/main/java/com/example/sportclub/controller/ClubController.java
@@ -9,15 +9,15 @@ public class ClubController {
 
     public List<Trainer> getTrainers() {
         List<Trainer> trainers = new ArrayList<>();
-        trainers.add(new Trainer(1L, "Иванов И.И.", "Фитнес", "89990001122"));
-        trainers.add(new Trainer(2L, "Петров П.П.", "Бокс", "89991112233"));
+        trainers.add(new Trainer(1L, "Гордеева А.С.", "Йога", "89990001122"));
+        trainers.add(new Trainer(2L, "Гордеев А.А.", "Бокс", "89991112233"));
         return trainers;
     }
 
     public List<Training> getSchedule() {
         List<Training> list = new ArrayList<>();
-        Trainer t = new Trainer(1L, "Иванов И.И.", "Фитнес", "89990001122");
-        list.add(new Training("Фитнес", "2025-05-20", "18:00", t));
+        Trainer t = new Trainer(1L, "Гордеева А.А.", "Йога", "89990001122");
+        list.add(new Training("Йога", "2025-05-20", "18:00", t));
         return list;
     }
 }
